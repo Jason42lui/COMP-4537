89cdb236c6f1ba41e985a86359eb4f9c6e10a442
JASON_LUI <Jason53lui@gmail.com>
2023-09-15 22:56:43 -0700

    making my string const

--- COMP4537/Labs/1/scripts/reader.js
+++ COMP4537/Labs/1/scripts/reader.js
@@ -1,3 +1,11 @@
+const arraykey_string = "arrayKey"
+const display_container_string = "display-container"
+const div_string = "div"
+const item_container_string = "display-item-container"
+const store_time_string = "stored-time"
+const DOM_string = "DOMContentLoaded"
+
+
 // Go back function
 function goBack() {
     window.history.back();
@@ -5,15 +13,15 @@ function goBack() {
 
 function updateDisplayContainer() {
     // Gets the stored data, parses it to an array and grabs the reference while clearing the content in the display container
-    let storedArray = JSON.parse(localStorage.getItem('arrayKey')); 
-    const displayContainer = document.getElementById('display-container'); 
+    let storedArray = JSON.parse(localStorage.getItem(arraykey_string)); 
+    const displayContainer = document.getElementById(display_container_string); 
     displayContainer.innerHTML = ""; 
 
     // if the storedArray is not empty and IS an array, then iterate through the object and display it in the display-item-container
     if (storedArray && Array.isArray(storedArray)) { 
         storedArray.forEach(function(itemValue) {  
-            const div = document.createElement('div'); 
-            div.classList.add('display-item-container'); 
+            const div = document.createElement(div_string); 
+            div.classList.add(item_container_string); 
             div.textContent = itemValue; 
             displayContainer.appendChild(div); 
         });
@@ -21,14 +29,14 @@ function updateDisplayContainer() {
 }
 
 // Every two seconds update the items in the display-item-container 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener(DOM_string, function() {
     updateDisplayContainer(); 
     setInterval(updateDisplayContainer, 2000); 
 });
 
 // Updates the item every two seconds.
 setInterval(function() {
-    let storedTime = document.getElementById("stored-time"); 
+    let storedTime = document.getElementById(store_time_string); 
     let currentTime = new Date().toLocaleTimeString(); 
     storedTime.textContent = currentTime; 
 }, 2000);
--- COMP4537/Labs/1/scripts/writer.js
+++ COMP4537/Labs/1/scripts/writer.js
@@ -1,17 +1,33 @@
+const div_string = "div"
+const item_container_string = "display-item-container"
+const textarea_string = "textarea"
+const button_string = "button"
+const remove_string = "Remove"
+const display_container_string = "display-container"
+const arraykey_string = "arrayKey"
+const input_string = "input"
+const text_string = "text"
+const input_message_string = "Press enter to add"
+const store_time_string = "stored-time"
+const click_string = "click"
+const keyup_string = "keyup"
+const enter_string = "Enter"
+
+
 // Object constructor for Note
 function Note() {
     // Creating the Note Item Element
-    this.container = document.createElement("div");  
-    this.container.classList.add("display-item-container");
-    this.textArea = document.createElement("textarea");
-    this.removeButton = document.createElement("button");
-    this.removeButton.innerHTML = "Remove";
-    this.removeButton.addEventListener("click", () => {
+    this.container = document.createElement(div_string);  
+    this.container.classList.add(item_container_string);
+    this.textArea = document.createElement(textarea_string);
+    this.removeButton = document.createElement(button_string);
+    this.removeButton.innerHTML = remove_string;
+    this.removeButton.addEventListener(click_string, () => {
         this.remove();
     });
     this.container.appendChild(this.textArea); 
     this.container.appendChild(this.removeButton); 
-    const displayContainer = document.getElementById("display-container");
+    const displayContainer = document.getElementById(display_container_string);
     displayContainer.appendChild(this.container); 
 
 
@@ -22,19 +38,11 @@ function Note() {
         this.removeFromLocalStorage();
     };
 
-    // Store to Local storage Method
-    this.storeToLocalStorage = function() {
-        let itemList = JSON.parse(localStorage.getItem('arrayKey')) || [];
-        itemList.push(this.textArea.value);
-        localStorage.setItem('arrayKey', JSON.stringify(itemList));
-        // console.log(itemList, "LOCAL STORE");
-    };
-
     // Remove from Local storage Method
     this.removeFromLocalStorage = function() {
-        let itemList = JSON.parse(localStorage.getItem('arrayKey')) || [];
+        let itemList = JSON.parse(localStorage.getItem(arraykey_string)) || [];
         itemList.splice(itemList.indexOf(this.textArea.value), 1);
-        localStorage.setItem('arrayKey', JSON.stringify(itemList));
+        localStorage.setItem(arraykey_string, JSON.stringify(itemList));
         // console.log(itemList, "ITEM REMOVED");
     };
 }
@@ -43,16 +51,16 @@ let itemList = []
 
 // Add Note function
 function addTextArea() {
-    let input = document.createElement("input");
-    input.type = "text";
-    input.placeholder = "Press enter to add";
+    let input = document.createElement(input_string);
+    input.type = text_string;
+    input.placeholder = input_message_string;
     
     // Add input to display container
-    const displayContainer = document.getElementById("display-container");
+    const displayContainer = document.getElementById(display_container_string);
     displayContainer.appendChild(input);
 
-    input.addEventListener("keyup", (event) => {
-        if (event.key === "Enter" && input.value.trim() !== "") {
+    input.addEventListener(keyup_string, (event) => {
+        if (event.key === enter_string && input.value.trim() !== "") {
             let note = new Note();
             note.textArea.value = input.value;
             input.remove();
@@ -63,18 +71,18 @@ function addTextArea() {
 // Every two seconds, the function updates the time and the itemList to check for modifications to the text items
 setInterval(function() {
     // Update itemList from local storage
-    itemList = JSON.parse(localStorage.getItem("arrayKey")) || [];
+    itemList = JSON.parse(localStorage.getItem(arraykey_string)) || [];
 
-    let storedTime = document.getElementById("stored-time"); 
+    let storedTime = document.getElementById(store_time_string); 
     let currentTime = new Date().toLocaleTimeString(); 
     storedTime.textContent = currentTime; 
 
-    let textareas = document.querySelectorAll("textarea"); 
+    let textareas = document.querySelectorAll(textarea_string); 
     textareas.forEach(function(textarea, index) { 
         itemList[index] = textarea.value; 
     });
 
-    localStorage.setItem('arrayKey', JSON.stringify(itemList)); 
+    localStorage.setItem(arraykey_string, JSON.stringify(itemList)); 
     // console.log(itemList, "THIS IS LOCAL"); 
 }, 2000);
 
@@ -86,7 +94,7 @@ function goBack() {
 
 // Render the page function (Incase there are Notes in the local storage)
 function renderItemsFromLocalStorage() {
-    let itemList = JSON.parse(localStorage.getItem('arrayKey')) || [];
+    let itemList = JSON.parse(localStorage.getItem(arraykey_string)) || [];
     itemList.forEach(function(item) {
         let note = new Note();
         note.textArea.value = item;
